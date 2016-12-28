8e6a8696d4d6fc5188ac38bcc6ef040052c94d4a
wangenyong <wangenyong@gmail.com>
2016-12-28 11:29:46 +0800

    在工程中使用butterknife

--- app/src/main/java/com/wangenyong/dsfarm/MainActivity.java
+++ app/src/main/java/com/wangenyong/dsfarm/MainActivity.java
@@ -3,11 +3,20 @@ package com.wangenyong.dsfarm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.roughike.bottombar.BottomBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.bottomBar) BottomBar bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
     }
 }
