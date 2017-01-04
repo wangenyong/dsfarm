cda5f821bf1533dea3ed17a14fd9d15c5cc6363e
wangenyong <wangenyong@gmail.com>
2017-01-04 13:48:55 +0800

    Create CustomView

---
+++ app/src/main/java/com/wangenyong/dsfarm/data/model/CustomView.java
@@ -0,0 +1,17 @@
+package com.wangenyong.dsfarm.data.model;
+
+/**
+ * Created by wangenyong on 2017/1/4.
+ */
+
+public class CustomView {
+    private String title;
+
+    public CustomView(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
--- app/src/main/java/com/wangenyong/dsfarm/module/home/HomeContract.java
+++ app/src/main/java/com/wangenyong/dsfarm/module/home/HomeContract.java
@@ -1,7 +1,10 @@
 package com.wangenyong.dsfarm.module.home;
 
+import com.wangenyong.dsfarm.data.model.CustomView;
 import com.wangenyong.dsfarm.module.base.BasePresenter;
 import com.wangenyong.dsfarm.module.base.BaseView;
+
+import java.util.List;
 
 /**
  * Created by wangenyong on 2017/1/4.
@@ -9,7 +12,7 @@ import com.wangenyong.dsfarm.module.base.BaseView;
 
 public class HomeContract {
     interface View extends BaseView {
-        void showCustomViews();
+        void showCustomViews(List<CustomView> customViews);
     }
 
     interface Presenter extends BasePresenter<View> {
