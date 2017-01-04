b4b809c38fb5ed6869e096788366a430ded3ef48
wangenyong <wangenyong@gmail.com>
2017-01-04 14:19:31 +0800

    Create DataManager

---
+++ app/src/main/java/com/wangenyong/dsfarm/data/DataManager.java
@@ -0,0 +1,10 @@
+package com.wangenyong.dsfarm.data;
+
+/**
+ * Created by wangenyong on 2017/1/4.
+ */
+
+public class DataManager {
+
+
+}
---
+++ app/src/main/java/com/wangenyong/dsfarm/data/local/FakeDataHelper.java
@@ -0,0 +1,8 @@
+package com.wangenyong.dsfarm.data.local;
+
+/**
+ * Created by wangenyong on 2017/1/4.
+ */
+
+public class FakeDataHelper {
+}
