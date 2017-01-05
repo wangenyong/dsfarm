b9d0fc081b9220220f8aef230f4157c67d9937de
wangenyong <wangenyong@gmail.com>
2017-01-05 14:15:20 +0800

    设置LevelView颜色

--- app/src/main/java/com/wangenyong/dsfarm/module/customview/LevelView.java
+++ app/src/main/java/com/wangenyong/dsfarm/module/customview/LevelView.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
+import android.graphics.RectF;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.wangenyong.dsfarm.R;
 
 /**
  * Created by wangenyong on 2017/1/5.
@@ -14,9 +17,12 @@ import android.view.View;
 
 public class LevelView extends View {
 
+    private Context mContext;
     private int mWidth, mHeight;
 
-    private Paint mPaint = new Paint();
+    private Paint mPaintB = new Paint();
+    private Paint mPaintF = new Paint();
+    private Paint mPaintT = new Paint();
 
     public LevelView(Context context) {
         this(context, null);
@@ -24,10 +30,25 @@ public class LevelView extends View {
 
     public LevelView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setColor(Color.BLACK);
-        mPaint.setAntiAlias(true);
-        mPaint.setStrokeWidth(10);
+        mContext = context;
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaintB.setStyle(Paint.Style.FILL_AND_STROKE);
+        mPaintB.setColor(ContextCompat.getColor(mContext, R.color.barBgLevelView));
+        mPaintB.setAntiAlias(true);
+        mPaintB.setStrokeWidth(10);
+
+        mPaintF.setStyle(Paint.Style.FILL);
+        mPaintF.setColor(ContextCompat.getColor(mContext, R.color.barLevelView));
+        mPaintF.setAntiAlias(true);
+
+        mPaintT.setStyle(Paint.Style.FILL);
+        mPaintT.setColor(Color.BLACK);
+        mPaintT.setAntiAlias(true);
+        mPaintT.setTextSize(50);
+
     }
 
     @Override
@@ -40,12 +61,13 @@ public class LevelView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawColor(Color.BLUE);
-        canvas.translate(mWidth / 2, mHeight / 2);
-        Path path = new Path();
-        path.lineTo(200, 200);
-        path.lineTo(200,0);
-        canvas.drawPath(path, mPaint);
+        canvas.drawColor(ContextCompat.getColor(mContext, R.color.bgLevelView));
+        canvas.translate(0, mHeight / 2);
+
+        RectF rectFB = new RectF(getPaddingLeft(),-10,mWidth-getPaddingRight(),10);
+        canvas.drawRoundRect(rectFB, 10, 10, mPaintB);
+        RectF rectFF = new RectF(getPaddingLeft(),-10,mWidth/3,10);
+        canvas.drawRoundRect(rectFF, 10, 10, mPaintF);
     }
 
 }
